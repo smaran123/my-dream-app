f92e966bcb6cc2395f61c21dff8fb834c682bda3
bhagath <bhagath@getcustomexposure.com>
2018-04-09 11:49:17 +0530

    creating components with the CLI & Nesting Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent
+    ServerComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule,
